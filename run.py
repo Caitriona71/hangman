d74b98229e088e37ceef3940f1092e127bf56d9e
Caitriona71 <caitrionaboyle@ymail.com>
2022-10-03 14:56:47 +0000

    Add a printed message inviting the user to play the hangman game

--- run.py
+++ run.py
@@ -3,10 +3,12 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
 
-print("Let's play hangman.")
+print("Let's play Hangman.")
 
 from words import word_list
 
+### Select a random word
 def get_a_word():
     word = random.choice(word_list)
     return word.upper()
+
